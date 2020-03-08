fbbacdb89a4149540d1ddf5d1c49d285ab301696
Harold <harolds98@live.be>
2020-03-09 00:50:21 +0100

    update javasever to jsp scriplet and adding jslt

--- JavaServer/src/controller/home.java
+++ JavaServer/src/controller/home.java
@@ -5,26 +5,23 @@ import sun.rmi.runtime.Log;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 import java.io.Console;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "home")
 public class home extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try (PrintWriter out = response.getWriter()) {
             Cookie[] cookies = request.getCookies();
             Cookie user = findCookie("user", cookies);
-            Cookie logout = findCookie("logout",cookies);
+            Cookie logout = findCookie("logout", cookies);
+            HttpSession session = request.getSession();
 
             String username = request.getParameter("user");
             String pwd = request.getParameter("pwd");
 
-            if(request.getParameter("logout") != null){
+            if (request.getParameter("logout") != null) {
                 logout.setValue("true");
                 user.setValue("");
 
@@ -39,9 +36,16 @@ public class home extends HttpServlet {
             if (!user.getValue().equals("")) {
                 RequestDispatcher dispatcher = request.getRequestDispatcher("/process1.jsp");
                 dispatcher.forward(request, response);
-            }
-            else if(LoginUser.validate(username, pwd) &&
-                    user.getValue().equals("")) {
+
+                // session management
+                if (request.getParameter("add") != null) {
+                    session.setAttribute(request.getParameter("attribute"),
+                            request.getParameter("value"));
+                } else if (request.getParameter("remove") != null) {
+                    session.removeAttribute(request.getParameter("attribute"));
+                }
+
+            } else if (LoginUser.validate(username, pwd) && user.getValue().equals("")) {
                 user.setValue(username);
                 logout.setValue("false");
                 response.addCookie(logout);
@@ -60,31 +64,17 @@ public class home extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         Cookie[] cookies = request.getCookies();
-        Cookie user = findCookie("user",cookies);
-        Cookie logout = findCookie("logout",cookies);
+        Cookie user = findCookie("user", cookies);
+        Cookie logout = findCookie("logout", cookies);
 
         log(user.getValue());
 
-//        // log out of the system
-//        if(request.getParameter("logout") != null) {
-//            if (logout.getValue().equals("false")) {
-//                logout.setValue("true");
-//            }
-//        }
-
         // user exist and is connected (thus cookies exist too)
-        if(!user.getValue().equals("") &&
-                logout.getValue().equals("false")){
+        if (!user.getValue().equals("") &&
+                logout.getValue().equals("false")) {
             RequestDispatcher dispatcher = request.getRequestDispatcher("/process1.jsp");
             dispatcher.forward(request, response);
-//        }
-//
-//        // user asked to be log out
-//         else if(logout.getValue().equals("true") &&
-//                request.getParameter("logout") != null){
-//            logout.setValue("false");
-//            user.setValue("");
-//
+
         } else {
             RequestDispatcher dispatcher = request.getRequestDispatcher("/login1.jsp");
             dispatcher.forward(request, response);
@@ -96,13 +86,13 @@ public class home extends HttpServlet {
 
     private Cookie findCookie(String name, Cookie[] cookies) {
         Cookie n = null;
-        for(Cookie c: cookies){
-            if(c.getName().equals(name)){
+        for (Cookie c : cookies) {
+            if (c.getName().equals(name)) {
                 n = c;
             }
         }
-        if(n == null){
-            n = new Cookie(name,"");
+        if (n == null) {
+            n = new Cookie(name, "");
         }
         return n;
     }
--- JavaServer/web/process1.jsp
+++ JavaServer/web/process1.jsp
@@ -1,4 +1,4 @@
-<%--
+<%@ page import="java.util.Enumeration" %><%--
   Created by IntelliJ IDEA.
   User: Harold
   Date: 04/03/2020
@@ -14,12 +14,16 @@
     <%
         String user = "";
         Cookie[] cookies = request.getCookies();
+
+        Enumeration<String> attributes = session.getAttributeNames();
+
         for(Cookie c: cookies) {
             if (c.getName().equals("user")) {
                 user = c.getValue();
             }
         }
     %>
+    <p> session ID: <%= session.getId() %> </p>
     <h1> you are logged in as <%= user %> </h1>
     <div class='button_form'>
         <form action="home" method="post">
@@ -29,5 +33,22 @@
             <input type="submit" value="logout" name="logout">
         </form>
     </div>
+    <div class='button_form'>
+        <form action="home" method="post">
+            Attribute : <input type='text' name='attribute'/>
+            Value : <input type='text' name='value'/>
+            <input type='submit' value='Add' name='add'/>
+            <input type='submit' value='Remove' name='remove'/>
+        </form>
+    </div>
+    <ul>
+        <%
+            while(attributes.hasMoreElements()){
+                String att = attributes.nextElement();
+                out.println("<li>" + att + " : " + session.getAttribute(att)
+                        + "</li>");
+            }
+        %>
+    </ul>
 </body>
 </html>
